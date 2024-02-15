f0bdaa58bde678ba53d65bbc0e5019ce3aa980b2
Artur Krasniewski <artur.krasniewski1@gmail.com>
2024-02-15 17:00:42 +0100

    single product page styles adjustments

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,36 +3,37 @@ import React from "react";
 
 const ProductPage = async ({ params }: { params: { id: string } }) => {
 	const res = await fetch(`https://naszsklep-api.vercel.app/api/products/${params.id}`);
-	const product = await res.json();
+	const product = (await res.json()) as ProductItemType;
 	return (
-		<article className="mx-auto max-w-screen-md rounded-lg bg-white p-4 shadow-lg">
-			<div className="mb-4">
+		<article className="justify-between rounded-lg bg-white p-4 shadow-lg md:flex md:p-7">
+			<div className="mb-4 md:mr-5">
 				<img src={product.image} alt={product.title} className="h-auto w-full rounded-lg" />
 			</div>
-			<div className="mb-4">
-				<div className="mb-2 flex items-center justify-between">
-					<h1 className="text-4xl font-bold">{product.title}</h1>
-					<p className="text-2xl text-green-600">{product.price / 100} zł</p>
+			<div className="mb-4 md:w-4/6">
+				<div className="mb-4">
+					<h1 className="mb-1 text-4xl font-bold">{product.title}</h1>
+					<p className="mb-4 text-xl font-light">{product.category}</p>
+					<p className="text-2xl text-green-600 md:text-3xl">{product.price / 100} zł</p>
 				</div>
-				<div className="flex items-center justify-between">
-					<div className="mb-2">
+				<div className="mb-4 items-center justify-between lg:flex lg:flex-row-reverse">
+					<div className="mb-4 lg:mb-0">
 						<span className="inline text-yellow-500">
 							{Array(Math.round(product.rating.rate)).fill("★").join("")}
 						</span>
 						<span className="text-gray-500">({product.rating.count} opinii)</span>
 					</div>
-					<button className="mt-2 rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+					<button className="rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
 						Dodaj do koszyka
 					</button>
 				</div>
+				<div className="mb-4">
+					<p className="font-semibold text-gray-700">{product.description}</p>
+				</div>
+				<div
+					className="text-gray-700"
+					dangerouslySetInnerHTML={{ __html: product.longDescription }}
+				/>
 			</div>
-			<section className="mb-4">
-				<p className="text-gray-700">{product.description}</p>
-			</section>
-			<section
-				className="mt-4 text-gray-700"
-				dangerouslySetInnerHTML={{ __html: product.longDescription }}
-			/>
 		</article>
 	);
 };
