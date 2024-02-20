847d9670ba62f73bf3ce367a14f2c8573b0bbaa1
Artur Krasniewski <artur.krasniewski1@gmail.com>
2024-02-20 22:40:40 +0100

    layout corrections

--- src/ui/organisms/ProductsList.tsx
+++ src/ui/organisms/ProductsList.tsx
@@ -9,7 +9,7 @@ type ProductsListProps = {
 const ProductsList = async ({ products }: ProductsListProps) => {
 	return (
 		<ul
-			className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+			className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4"
 			data-testid="products-list"
 		>
 			{products.map((product) => (
