c2bc7057172a1cf4ad3743483df4bf92a57af82c
Artur Krasniewski <artur.krasniewski1@gmail.com>
2024-02-20 22:12:17 +0100

    product card image adjustments

--- src/ui/atoms/ProductCardImage.tsx
+++ src/ui/atoms/ProductCardImage.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import { placeholder } from "@/helpers/placeholder";
-import { getPlaiceholder } from "plaiceholder";
-const probe = require("probe-image-size");
 
 const ProductCardImage = async ({ src, alt }: { src: string; alt: string }) => {
-	const blur = await placeholder(src);
-	const imgSize = await probe(src);
 	return (
-		<div className="mb-4 w-full sm:h-32">
-			<Image
-				src={src}
-				alt={alt}
-				width={imgSize.width}
-				height={imgSize.height}
-				quality={40}
-				className="mx-auto h-full w-auto rounded-lg sm:max-h-36"
-				placeholder="blur"
-				blurDataURL={blur}
-			/>
+		<div className="relative mb-4 h-48 w-auto">
+			<Image src={src} alt={alt} fill className="mx-auto h-auto w-auto rounded-lg object-contain" />
 		</div>
 	);
 };
