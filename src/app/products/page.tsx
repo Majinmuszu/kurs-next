e43bea37e92147c89fa77810f05fca0a642be269
Artur Krasniewski <artur.krasniewski1@gmail.com>
2024-02-20 23:54:21 +0100

    added gql on /products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,17 @@
-import { getProductsWithOffset } from "@/api/productApi";
-import { ProductItemType } from "@/types/product";
+import { executeGraphql } from "@/api/api";
+import { ProductsGetListDocument } from "@/gql/graphql";
 import Pagination from "@/ui/molecules/Pagination";
 import ProductsList from "@/ui/organisms/ProductsList";
 import React from "react";
 
 const ProductsPage = async ({ params }: { params: { pageNumber: string } }) => {
-	const page = params.pageNumber ? parseInt(params.pageNumber) : 1;
-	const offset = (page - 1) * 20;
-	const products = await getProductsWithOffset(offset);
+	// const page = params.pageNumber ? parseInt(params.pageNumber) : 1;
+	// const offset = (page - 1) * 20;
+	const products = await executeGraphql(ProductsGetListDocument);
 	return (
 		<section>
 			<ProductsList products={products} />
-			<Pagination currentPage={page} />
+			{/* <Pagination currentPage={page} /> */}
 		</section>
 	);
 };
