01998d9cbe11a225ff8ff45d8a9dbc5471445e75
Artur Krasniewski <artur.krasniewski1@gmail.com>
2024-02-21 12:36:26 +0100

    categories page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { ProductsGetListDocument } from "@/gql/graphql";
 import { ProductsList } from "@/ui/organisms/ProductsList";
 
 export default async function Home() {
-	const res = await executeGraphql(ProductsGetListDocument, { offset: 0 });
+	const { products } = await executeGraphql(ProductsGetListDocument, { offset: 0 });
 	return (
 		<section>
-			<ProductsList products={res} />
+			<ProductsList products={products.data} />
 		</section>
 	);
 }
