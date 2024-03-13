cf566c02d277e9cc0ec5a09b96d223b8111788b3
Artur Krasniewski <artur.krasniewski1@gmail.com>
2024-03-13 22:34:52 +0100

    redirect products to first page paginated

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const nextConfig = {
 	images: {
 		domains: ["naszsklep-api.vercel.app", "static-ourstore.hyperfunctor.com"],
 	},
+	async redirects() {
+		return [
+			{
+				source: "/products",
+				destination: "/products/1",
+				permanent: false,
+			},
+		];
+	},
 };
 
 export default nextConfig;
